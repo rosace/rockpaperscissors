368975e4d256856c934379535862d7040ca27b20
rosa <rosa.calmopablo@ousd.us>
2018-10-26 01:21:12 +0000

    Basic Rock Papers

--- js/script.js
+++ js/script.js
@@ -5,6 +5,37 @@
 //GLOBAL VARIABLES
 /* global $ */
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+
+function getComputerChoice() {
+   var randomNo = getRandomInt(3)
+   var computerChoice;  
+   
+   if (randomNo == 0) {
+    computerChoice = "Rock"
+    } else if (randomNo == 1) {
+        computerChoice = "Scissors"
+    } else if (randomNo == 2) {
+        computerChoice = "Paper"
+    }
+    
+    return computerChoice
+}
+
+
+var userChoiceDisplay = document.getElementById("userChoice")
+var computerChoiceDisplay = document.getElementById("computerChoice")
+var userChoiceInput = document.getElementById("input")
+
+$("#shoot").click(function() {
+    var computerChoice = getComputerChoice()
+    userChoiceDisplay.textContent = userChoiceInput.value
+    computerChoiceDisplay.textContent = computerChoice
+})
 
 
 // DOCUMENT READY FUNCTION BELOW
